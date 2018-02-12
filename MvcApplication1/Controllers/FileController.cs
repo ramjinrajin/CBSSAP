0972d608edcba7521fc5d9caac4c38a926d74e63
ramjin.rajin@hotmail.com <ramjin.rajin@hotmail.com>
2018-02-13 00:52:09 +0530

    Dev1

---
+++ MvcApplication1/Controllers/FileController.cs
@@ -0,0 +1,36 @@
+﻿using MvcApplication1.Models.File;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class FileController : Controller
+    {
+        //
+        // GET: /File/
+
+        public ActionResult Index()
+        {
+            List<FileModel> ListFiles = new List<FileModel>()
+            {
+                new FileModel
+                {
+                    FileId=1,Name="Ramjin",Path="sdaasd"
+                },
+                new FileModel
+                {
+                    FileId=1,Name="samjin",Path="sdaasd"
+                },
+                  new FileModel
+                {
+                    FileId=1,Name="Ramesh",Path="sdaasd"
+                }
+            };
+            return View(ListFiles);
+        }
+
+    }
+}
---
+++ MvcApplication1/Models/File/FileModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models.File
+{
+    public class FileModel
+    {
+        public int FileId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Path { get; set; }
+
+    }
+}
